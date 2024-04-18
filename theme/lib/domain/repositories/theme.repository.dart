933020f1d69f89122ff19d6d921c4fb0950c3c50
David <davidkisbeygreen145@gmail.com>
2024-04-18 13:50:45 +0100

    feat(dataSource): added assets dataSource

--- theme/lib/domain/repositories/theme.repository.dart
+++ theme/lib/domain/repositories/theme.repository.dart
@@ -1,8 +1,5 @@
 import 'package:theme/data/models/theme/theme.dart';
 import 'package:theme/data/repositories/theme.repository.dart';
-import 'package:theme/utils/loggers.dart';
-import 'package:utilities/logger/logger.dart';
-
 import '/data/repositories/_repositories.dart';
 import 'base.repository.dart';
 
--- utilities/lib/data_sources/local/assets.dart
+++ utilities/lib/data_sources/local/assets.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
-import 'package:theme/utils/loggers.dart';
 import 'package:utilities/data_sources/source.dart';
 import 'package:utilities/logger/logger.dart';
 import 'package:utilities/utils/loggers.dart';
+import 'package:collection/collection.dart';
 
 /// [AssetsDataSource] is a wrapper class for [rootBundle] which implements [DataSource]
 class AssetsDataSource<T> implements DataSource<T> {
@@ -36,26 +36,38 @@ class AssetsDataSource<T> implements DataSource<T> {
 
   @override
   Future<T?> get(String id) async {
-    final rootBundleKeyWithId = rootBundleKey;
-    final data = await _tryLoadJson(rootBundleKeyWithId);
-
-    return data;
+    final data = await _tryLoadJson(rootBundleKey, id: id);
+    if (data.isEmpty) return null;
+    return data.first;
   }
 
-  Future<T?> _tryLoadJson(String path) async {
+  Future<List<T?>> _tryLoadJson(String path, {String? id}) async {
     try {
       final jsonString = await rootBundle.loadString(path);
+      final response = json.decode(jsonString);
+      if (response is List) {
+        AppLogger.print("Asset response List length: ${response.length}", [UtilitiesLoggers.assetsDataSource]);
+        final data = response.map((e) => e as Map<String, dynamic>).toList();
+        if (id != null) {
+          final result = data.firstWhereOrNull((element) => element['id'] == id);
+          return [convertDataTypeFromMap(result ?? <String, dynamic>{})];
+        }
+        return data.map(convertDataTypeFromMap).toList();
+      }
+      AppLogger.print("Asset response Map from path: $path", [UtilitiesLoggers.assetsDataSource]);
       final data = convertDataTypeFromMap(json.decode(jsonString) as Map<String, dynamic>);
-      return data;
+      return [data];
     } catch (e) {
       AppLogger.print("Error in parsing json for Assets $T: $e", [UtilitiesLoggers.assetsDataSource], type: LoggerType.error);
     }
-    return null;
+    return [];
   }
 
   @override
   Future<List<T?>> getAll() async {
-    throw UnimplementedError();
+    final data = await _tryLoadJson(rootBundleKey);
+
+    return data;
   }
 
   @override
