308c33bf4dac0dd35b2d411ae94e962b3034422a
Krispy145 <davidkisbeygreen145@gmail.com>
2024-04-26 20:14:03 +0400

    polish: ui polishes

--- forms/lib/presentation/components/base/form_field.dart
+++ forms/lib/presentation/components/base/form_field.dart
@@ -5,11 +5,11 @@ import "package:utilities/widgets/load_state/state_widget.dart";
 import "store.dart";
 
 // ignore: strict_raw_type
-abstract class BaseFormField<T extends BaseFormFieldStore>
-    extends StatelessWidget {
-  const BaseFormField({super.key, required this.store});
+abstract class BaseFormField<T extends BaseFormFieldStore> extends StatelessWidget {
+  const BaseFormField({super.key, required this.store, this.showTitle = true});
 
   final T store;
+  final bool showTitle;
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +30,11 @@ abstract class BaseFormField<T extends BaseFormFieldStore>
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 24, bottom: 8),
-              child: Text(store.title, style: context.textTheme.titleSmall),
-            ),
+            if (showTitle)
+              Padding(
+                padding: const EdgeInsets.only(top: 24, bottom: 8),
+                child: Text(store.title, style: context.textTheme.titleSmall),
+              ),
             buildField(context),
           ],
         );
