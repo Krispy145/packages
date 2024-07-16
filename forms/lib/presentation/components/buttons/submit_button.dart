16ba9c4a81bde451514f6893770892920528637b
Krispy145 <davidkisbeygreen145@gmail.com>
2024-07-16 16:00:07 +0400

    feat: submit button added for forms

---
+++ forms/lib/presentation/components/buttons/submit_button.dart
@@ -0,0 +1,52 @@
+import "package:flutter/material.dart";
+import "package:forms/presentation/models/reactive_store.dart";
+import "package:reactive_forms/reactive_forms.dart";
+import "package:utilities/data/sources/source.dart";
+import "package:utilities/helpers/extensions/build_context.dart";
+import "package:utilities/snackbar/configuration.dart";
+
+class ReactiveFormSubmitButton<T> extends StatelessWidget {
+  const ReactiveFormSubmitButton({
+    super.key,
+    required this.store,
+    this.buttonText = "Submit",
+    this.successMessage,
+    this.errorMessage,
+  });
+
+  final ReactiveFormsModelStore<T> store;
+  final String buttonText;
+  final String? successMessage;
+  final String? errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return ReactiveFormConsumer(
+      builder: (context, form, w) => Opacity(
+        opacity: form.valid ? 1 : 0.5,
+        child: ElevatedButton(
+          onPressed: form.valid
+              ? () async {
+                  await store.submitPressed().then((result) {
+                    if (result == RequestResponse.success && successMessage != null) {
+                      context.showSnackbar(
+                        configuration: SnackbarConfiguration.confirmation(
+                          title: successMessage!,
+                        ),
+                      );
+                    } else if (errorMessage != null) {
+                      context.showSnackbar(
+                        configuration: SnackbarConfiguration.error(
+                          title: errorMessage!,
+                        ),
+                      );
+                    }
+                  });
+                }
+              : null,
+          child: Text(buttonText),
+        ),
+      ),
+    );
+  }
+}
