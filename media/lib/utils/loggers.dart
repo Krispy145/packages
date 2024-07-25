341e2849c15014fb9bab7bdf369bd077c4678d6f
Krispy145 <davidkisbeygreen145@gmail.com>
2024-07-25 18:04:52 +0100

    feat: media package added

---
+++ media/lib/utils/loggers.dart
@@ -0,0 +1,14 @@
+/// [MediaLoggers] class responsible for storing base feature references.
+/// Used to determine which features are enabled for logging.
+enum MediaLoggers {
+  /// [media] is the feature reference for media.
+  media,
+
+  /// [youtube] is the feature reference for youtube.
+  youtube,
+
+  /// [presentationLayer] is the PresentationLayer feature
+  presentationLayer,
+
+  /// LOGGER FEATURE END
+}
