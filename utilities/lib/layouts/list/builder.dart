e2fbf7c994754bbd858cb9daa979e506eaca85b6
Krispy145 <davidkisbeygreen145@gmail.com>
2024-08-15 12:26:41 +0100

    fix: List builder padding fixed

--- utilities/lib/layouts/list/builder.dart
+++ utilities/lib/layouts/list/builder.dart
@@ -111,29 +111,26 @@ class ListBuilder<T> extends StatelessWidget {
         },
         loadedBuilder: (context) {
           print("LOADED BUILDER");
-          final contents = Padding(
-            padding: !slivers ? padding : EdgeInsets.zero,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: shrinkWrap ? MainAxisSize.min : MainAxisSize.max,
-              children: [
-                if (header != null) ...[header!, Sizes.m.spacer()],
-                if (shrinkWrap)
-                  Observer(
+          final contents = Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: shrinkWrap ? MainAxisSize.min : MainAxisSize.max,
+            children: [
+              if (header != null) ...[header!, Sizes.m.spacer()],
+              if (shrinkWrap)
+                Observer(
+                  builder: (context) {
+                    return buildView(store.showLoadingSpinnerAtBottom);
+                  },
+                )
+              else
+                Expanded(
+                  child: Observer(
                     builder: (context) {
                       return buildView(store.showLoadingSpinnerAtBottom);
                     },
-                  )
-                else
-                  Expanded(
-                    child: Observer(
-                      builder: (context) {
-                        return buildView(store.showLoadingSpinnerAtBottom);
-                      },
-                    ),
                   ),
-              ],
-            ),
+                ),
+            ],
           );
           return stackedWidgets != null
               ? Stack(
